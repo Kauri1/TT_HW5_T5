05147f7a15244527c07515de8a6f5f0e18b629a6
Kauri <kauriremm@gmail.com>
2025-03-31 17:16:42 +0300

    deleted wrong file

--- lg3-tiim-q/src/main/resources/META-INF/initial-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO StockItems (name, description, price, quantity) VALUES ('Lays chips', 'Potato chips', 11.0, 5);
-INSERT INTO StockItems (name, description, price, quantity) VALUES ('Chupa-chups', 'Sweets', 8.0, 8);
-INSERT INTO StockItems (name, description, price, quantity) VALUES ('Frankfurters', 'Beer sauseges', 15.0, 12);
-INSERT INTO StockItems (name, description, price, quantity) VALUES ('Free Beer', 'Students delight', 0.0, 100);
